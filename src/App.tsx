6df89d2aff3b91ea56e235acd20beb433c86d413
Кирилл <kirill@MacBook-Pro-Kirill.local>
2025-09-28 19:59:21 +0300

    React header

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from './components/Header/Header'; // Импортируем нашу шапку
+
+function App() {
+    return (
+        <>
+            <Header />
+            {/* Здесь будут отображаться другие страницы (контент) */}
+        </>
+    );
+}
+
+export default App;
