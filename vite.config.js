1b6577baa780acd67afc52d6471671888d9af03e
Modory <dashkarni@gmail.com>
2025-11-05 21:21:18 +0300

    several updates

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 
 export default ({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
-  const apiUrl = env.VITE_API_URL || 'http://localhost:3000/'
+  const apiUrl = env.VITE_API_URL || 'http://localhost:4000/'
 
   const secure = apiUrl.startsWith('https://')
 
