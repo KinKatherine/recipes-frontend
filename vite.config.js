db8c61513503dcfeb7e3ebe5fe32e2e2deb2b73f
KinKatherine <katunja2005@tut.by>
2025-11-05 18:24:37 +0100

    feat: add Docker file and environment vars

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,23 @@
-import { defineConfig } from 'vite'
+/* eslint-env node */
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server:{
-    proxy: {
-      '/api': {
-        target: 'http://26.242.102.42:8080', 
-        changeOrigin: true, 
-        secure: false, 
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  const apiUrl = env.VITE_API_URL || 'http://localhost:3000/'
+
+  const secure = apiUrl.startsWith('https://')
+
+  return defineConfig({
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/api': {
+          target: apiUrl,
+          changeOrigin: true,
+          secure,
+        },
       },
-    }
-  } 
-});
+    },
+  })
+}
